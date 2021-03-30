0cd1a172f3f0f2447c987837113965b874574fd9
Sanjay Kumar <sanjay.k@uniapply.com>
2021-03-30 18:32:32 +0530

    WIP

--- lib/scroll_with_tabs.dart
+++ lib/scroll_with_tabs.dart
@@ -1,7 +1,57 @@
 library scroll_with_tabs;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:scroll_with_tabs/models/TabData.dart';
+
+typedef ScrollIndexedWidgetBuilder = Widget Function(
+    BuildContext context, int index, GlobalKey key);
+
+class ScrollWithTabs extends StatefulWidget {
+  final List<TabData> tabDataList;
+  final ScrollIndexedWidgetBuilder itemBuilder;
+
+  ScrollWithTabs({this.tabDataList, this.itemBuilder});
+
+  @override
+  _ScrollWithTabsState createState() => _ScrollWithTabsState();
+}
+
+class _ScrollWithTabsState extends State<ScrollWithTabs> {
+  ScrollController scrollController;
+  final List<GlobalKey> keys = [];
+  final List<Widget> widgets = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: widget.tabDataList.length,
+        child: NestedScrollView(
+          headerSliverBuilder:
+              (BuildContext context, bool innerBoxIsScrolled) {},
+          body: Builder(
+            builder: (context) {
+              scrollController = PrimaryScrollController.of(context);
+              scrollController.addListener(scrollListener);
+              return ListView(
+                children: buildChildren(context),
+              );
+            },
+          ),
+        ));
+  }
+
+  void scrollListener() async {}
+
+  List<Widget> buildChildren(BuildContext context) {
+    if (widgets.length == 0) {
+      for (int index = 0; index < widget.tabDataList.length; index++) {
+        final key = GlobalKey();
+        widgets.add(widget.itemBuilder?.call(context, index, key));
+        keys.add(key);
+      }
+    }
+    return widgets;
+  }
 }
