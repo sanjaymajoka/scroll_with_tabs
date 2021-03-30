657c9d04d40a06db67c81cb1cd841a7e18ba2e29
Sanjay Kumar <sanjay.k@uniapply.com>
2021-03-30 14:35:50 +0530

    example added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## [0.0.1] - TODO: Add release date.
+## [0.0.1] - 30-03-2021
 
-* TODO: Describe initial release.
+* A description for people who will use that package or version.
---
+++ example/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'HomePage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
